d0cd648e90c9cb0b1110b4b1aa5910d459afb462
kevinc2465 <chenk5104@gmail.com>
2024-03-25 10:16:56 -0400

    prettier?

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,43 +1,43 @@
 const User = require("../Models/User");
 
 exports.createUser = async (req, res) => {
-    try{
-        const user = new User(req.body);
-        await user.save();
-        res.json(user);
-    } catch(error) {
-        res.status(500).json(error);
-    }
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 exports.updateUser = async (req, res) => {
-    try {
-      const user = await User.findById(req.params.id);
-      const updates = Object.keys(req.body);
-      updates.forEach((update) => (user[update] = req.body[update]));
-      await user.save();
-      res.json(user);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  };
+  try {
+    const user = await User.findById(req.params.id);
+    const updates = Object.keys(req.body);
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
+    res.json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
-  exports.deleteUser = async (req, res) => {
-    try{
-        const user = await User.findById(req.params.id);
-        if(user) {
-            await User.deleteOne({username: user.username});
-        }
-    } catch(error) {
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      await User.deleteOne({ username: user.username });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+`  exports.getRanking = async (req, res) => {
+      try {
+        const users = await User.find().sort({ score: -1 });
+        res.json(users);
+      } catch (error) {
         res.status(500).json(error);
-    }
-  };
-  
-  exports.getRanking = async (req, res) => {
-    try {
-      const users = await User.find().sort({ score: -1 });
-      res.json(users);
-    } catch (error) {
-      res.status(500).json(error);
-    }
-  };
+      }
+    };`;
